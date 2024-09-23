ff6c348930c69b3b40120b81ced60b390e1ebd06
rdev0010 <128361490+rdev0010@users.noreply.github.com>
2024-09-23 23:51:24 +1000

    reuploaded folders

---
+++ js/vega.js
@@ -0,0 +1,4 @@
+var spec2 = "js/choropleth_map.vg.json";
+  vegaEmbed('#choropleth_map', spec2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  }).catch(console.error);
