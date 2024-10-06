20ea611ee22957b6da204a43bb29408afae83c85
rdev0010 <128361490+rdev0010@users.noreply.github.com>
2024-10-07 02:58:59 +1100

    Delete js/vega.js

--- js/vega.js
+++
@@ -1,9 +0,0 @@
-var spec1 = "js/choropleth_map.vg.json";
-  vegaEmbed('#choropleth_map', spec1).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-  }).catch(console.error);
-
-  var spec2 = "js/trends.vg.json";
-  vegaEmbed('#choropleth_map', spec2).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-  }).catch(console.error);
