a42fdde4e532497d45f604c35e5ed4ff6b33578e
rdev0010 <128361490+rdev0010@users.noreply.github.com>
2024-10-06 23:52:01 +1100

    Update vega.js

--- js/vega.js
+++ js/vega.js
@@ -4,6 +4,6 @@ var spec1 = "js/choropleth_map.vg.json";
   }).catch(console.error);
 
   var spec2 = "js/trends.vg.json";
-  vegaEmbed('#trends', spec2).then(function(result) {
+  vegaEmbed('#choropleth_map', spec2).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
   }).catch(console.error);
